bbfb5b08b4077baf3b9a0bea55af9ec4095b87ac
KazachenokI <sovuh.vetrodui@gmail.com>
2023-01-20 16:02:43 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,119 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.12193815723648527,
+        "pitch": -0.09435921303741068,
+        "fov": 1.2316530311887501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.021815722352840794,
+          "pitch": 0.23918482458834767,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.9286701600901992,
+        "pitch": -0.04570989107617862,
+        "fov": 1.2316530311887501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0765698082805635,
+          "pitch": 0.23126902598892052,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 0.07260396745281916,
+          "pitch": 0.37897029313801767,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.233053724810471,
+        "pitch": 0.07997440081092577,
+        "fov": 1.2316530311887501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1027333145364455,
+          "pitch": 0.35430545327976937,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Санта Минск Мир ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
